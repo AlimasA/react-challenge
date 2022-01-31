e09e730b98d0a89b78725ff80e0fa713fe6a3c1b
Andrius Alimas <andrius.alimas@dxc.com>
2022-01-31 22:02:27 +0000

    Create MenuItems.js
    Created exportable data array of object MenuItems that will be used in Navbar component

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'About us',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Travel',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Feedback',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gazetteer',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact us',
+        url: "#",
+        cName: 'nav-links'
+    }
+]
