1f339b9588d5e7b398553eb3d27299f4c3af6331
Andrius Alimas <andrius.alimas@dxc.com>
2022-01-31 22:00:52 +0000

    Create Navbar.js
    Created NavBar functional component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import {MenuItems} from './MenuItems';
+
+function Navbar(props) {
+  return (
+      <nav className='NavbarItems'>
+          <h1 className="navbar-logo">React</h1>
+          <div className='menu-icon'>
+            
+          </div>
+          <ul>
+             
+          </ul>
+      </nav>
+  );
+}
+
+Navbar.propTypes = {};
+
+export default Navbar;
+
+
