73d82f37055a62491d90dfcea1be69723115b215
Andrius Alimas <andrius.alimas@dxc.com>
2022-02-01 11:18:48 +0000

    Update App.js
    Created Bootstrap Navbar with collapsible component on small screen appear hamburger icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React from 'react';
 import logo from './andrita_logo.svg';
 import { Container } from "./components/styles/Container.styled";
 import 'bootstrap/dist/css/bootstrap.css';
-import { Navbar} from "react-bootstrap";
+import { Nav, Navbar, NavLink} from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-     <Navbar bg="danger" variant="dark">
+     <Navbar bg="warning" variant="light" fixed="bottom" expand="sm">
         <Navbar.Brand>
             <img src={logo} alt=""  />
         </Navbar.Brand>
+
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+                    <Nav>
+          <Nav.Link href='#'>About us</Nav.Link>
+          <Nav.Link href='#'>Travel</Nav.Link>
+          <Nav.Link href='#'>Feedback</Nav.Link>
+          <Nav.Link href='#'>Gazetteer</Nav.Link>
+          <Nav.Link href='#'>Contact us</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+
      </Navbar>
        <Container>
           <h1>Hello App</h1>
