0e73d28f1cae8ed98efebc04ba45f2cdd9be2ac8
Andrius Alimas <andrius.alimas@dxc.com>
2022-02-01 08:38:45 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
+import logo from './andrita_logo.svg';
+import { Container } from "./components/styles/Container.styled";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar} from "react-bootstrap";
+
+const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+     <Navbar bg="danger" variant="dark">
+        <Navbar.Brand>
+            <img src={logo} alt=""  />
+        </Navbar.Brand>
+     </Navbar>
+       <Container>
+          <h1>Hello App</h1>
+      </Container>
     </>
-  );
+  )
 }
 
 export default App;
